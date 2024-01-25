256220954f720d276d12721462b253b3db6b2419
TohirDev <isakulovtohir902@gmail.com>
2024-01-25 17:14:48 +0500

    navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,29 @@ import Container from "./Container";
 
 const Navbar = () => {
   return (
-    <>
-    </>
+    <Container>
+      <nav className="flex items-center justify-between font-open-sans py-5">
+        <h1>logo</h1>
+        <ul className="flex items-center justify-between">
+          <li className="mx-5 nav-link nav-link-fade-up">
+            <span className="font-bold mr-2">01</span>home
+          </li>
+          <li className="mx-5 nav-link nav-link-fade-up">
+            <span className="font-bold mr-2">02</span>writing
+          </li>
+          <li className="mx-5 nav-link nav-link-fade-up">
+            <span className="font-bold mr-2">03</span>notes
+          </li>
+          <li className="mx-5 nav-link nav-link-fade-up">
+            <span className="font-bold mr-2">04</span>about
+          </li>
+          <li className="mx-5 nav-link nav-link-fade-up">
+            <div className="bg-[silver] w-[50px] h-[50px] rounded-[50%] grid place-items-center	">
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </Container>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "open-sans": ['"Open Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
