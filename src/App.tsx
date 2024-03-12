ae9f35dd521fbe1bb1a4c2412a062768b532717a
TohirDev <isakulovtohir902@gmail.com>
2024-03-12 10:51:17 +0500

    feat: all routes removed on App file and changed to Coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/Home";
-import AboutPage from "./pages/AboutPage";
-import Blog from "./pages/Writing";
-import Contact from "./pages/Contact";
+import ComingSoon from "./pages/ComingSoon";
 
 function App() {
-  return (
-    // <Routes>
-    //   <Route path="/" element={<HomePage />} />
-    //   <Route path="/about" element={<AboutPage />} />
-    //   <Route path="/blog" element={<Blog />} />
-    //   <Route path="/contact" element={<Contact />} />
-    // </Routes>
-    <>coming soon</>
-  );
+  return <ComingSoon />;
 }
 
 export default App;
+
+// import { Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/Home";
+// import AboutPage from "./pages/AboutPage";
+// import Blog from "./pages/Writing";
+// import Contact from "./pages/Contact";
+// <Routes>
+//   <Route path="/" element={<HomePage />} />
+//   <Route path="/about" element={<AboutPage />} />
+//   <Route path="/blog" element={<Blog />} />
+//   <Route path="/contact" element={<Contact />} />
+// </Routes>
