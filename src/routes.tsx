3b0d34d218e2cd1e4dbd32c057a306c4338255f1
TohirDev <isakulovtohir902@gmail.com>
2024-05-29 11:44:41 +0500

    updates

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,14 @@
-import { useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 import TStore from "./tstore";
 import Tui from "./tui";
 import { sideNavDetails } from "./constants";
 
 export const Router = () => {
   return useRoutes([
+    {
+      path: "/",
+      element: <Navigate to="/docs" />,
+    },
     {
       path: "/",
       element: <Tui />,
--- src/tui/docs/index.tsx
+++ src/tui/docs/index.tsx
@@ -1,101 +1,22 @@
 import { BreadCrumb } from "@/components/BreadCrumb";
 import { changeTitle } from "@/globals";
+import { Typography } from "beauty-ui-components";
 
 function Docs() {
   changeTitle("Introduction | Beauty/ui");
   return (
     <div>
       <BreadCrumb titles={["Docs", "Introduction"]} />
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
-      <h1>Docs</h1>
+      <Typography variant="h1">Introduction</Typography>
+      <Typography variant="p" style={{ width: "50%", marginTop: "30px" }}>
+        Welcome to <b>Beauty/ui</b>. This library has been thoughtfully
+        designed to provide a set of reusable and customizable UI
+        components tailored to meet the specific needs of our projects.
+        While it may not boast the extensive feature set of comprehensive
+        libraries like Material-UI (MUI) or Ant Design, it offers a
+        streamlined and efficient solution for building user interfaces
+        with simplicity and flexibility.
+      </Typography>
     </div>
   );
 }
