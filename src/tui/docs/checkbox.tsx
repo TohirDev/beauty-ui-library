7e58d70035c7cc6a3eeaa00751f8e057f10728b4
TohirDev <isakulovtohir902@gmail.com>
2024-07-01 14:16:12 +0500

    checkbox created

---
+++ src/tui/docs/checkbox.tsx
@@ -0,0 +1,11 @@
+import { changeTitle } from "@/globals";
+
+export const DocsCheckbox: React.FC = () => {
+  changeTitle("Docs | Checkbox");
+
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+};
