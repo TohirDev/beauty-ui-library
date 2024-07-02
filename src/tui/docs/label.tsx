8be48aa0f20e2d53d9176c9fcb3425f87e10e69c
TohirDev <isakulovtohir902@gmail.com>
2024-07-02 18:42:00 +0500

    label updated

--- src/tui/docs/label.tsx
+++ src/tui/docs/label.tsx
@@ -1,10 +1,27 @@
+import { BreadCrumb } from "@/components/BreadCrumb";
+import { Demo } from "@/components/demo";
 import { changeTitle } from "@/globals";
+import { Label, Typography } from "beauty-ui-components";
 
 export const DocsLabel: React.FC = () => {
   changeTitle("Docs | Label");
+  const code = `<Label>This is Label</Label>`;
   return (
     <div>
-      <h1>hi</h1>
+      <BreadCrumb titles={["Docs", "Label"]} />
+      <Typography variant="h2" sx={{ marginTop: "10px" }}>
+        Label
+      </Typography>
+      <Typography variant="p" sx={{ marginTop: "10px" }}>
+        Styles for headings, paragraphs and more.
+      </Typography>
+
+      <Typography variant="h3" sx={{ marginTop: "40px" }}>
+        Label
+      </Typography>
+      <Demo code={code}>
+        <Label>This is Label</Label>
+      </Demo>
     </div>
   );
 };
