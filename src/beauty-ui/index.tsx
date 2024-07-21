52ab2019f593623fef4929219e11c3712d702d95
TohirDev <isakulovtohir902@gmail.com>
2024-07-21 15:53:07 +0500

    changes
    ;

--- src/beauty-ui/index.tsx
+++ src/beauty-ui/index.tsx
@@ -2,14 +2,14 @@ import { SideNav } from "@/components/Sidenav";
 import { Outlet } from "react-router-dom";
 
 function BeautyUI() {
-  return (
-    <div className="flex">
-      <SideNav />
-      <div className="content">
-        <Outlet />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex">
+            <SideNav />
+            <div className="content">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default BeautyUI;
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -3,48 +3,50 @@ import { sideNavDetails } from "@/constants";
 import { Typography } from "beauty-ui-components";
 
 export const SideNav = () => {
-  return (
-    <div className="sidebar">
-      {sideNavDetails.map((item) => {
-        return (
-          <div key={item.title}>
-            {item.highlight ? (
-              <Typography variant="large">{item.title}</Typography>
-            ) : (
-              <NavLink to={item.path || "#"} key={item.title}>
-                <Typography
-                  variant="muted"
-                  sx={{
-                    fontSize: "18px",
-                    margin: "10px 0",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  {item.title}
-                  {item.new && (
-                    <Typography
-                      variant="small"
-                      sx={{
-                        marginLeft: "5px",
-                        backgroundColor: "#B6F837",
-                        color: "black",
+    return (
+        <div className="sidebar">
+            {sideNavDetails.map((item) => {
+                return (
+                    <div key={item.title}>
+                        {item.highlight ? (
+                            <Typography variant="large">
+                                {item.title}
+                            </Typography>
+                        ) : (
+                            <NavLink to={item.path || "#"} key={item.title}>
+                                <Typography
+                                    variant="muted"
+                                    sx={{
+                                        fontSize: "18px",
+                                        margin: "10px 0",
+                                        display: "flex",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    {item.title}
+                                    {item.new && (
+                                        <Typography
+                                            variant="small"
+                                            sx={{
+                                                marginLeft: "5px",
+                                                backgroundColor: "#B6F837",
+                                                color: "black",
 
-                        borderRadius: "0.375rem",
-                        textAlign: "center",
-                        display: "block",
-                        padding: "0.25rem 0.5rem",
-                      }}
-                    >
-                      New
-                    </Typography>
-                  )}
-                </Typography>
-              </NavLink>
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
+                                                borderRadius: "0.375rem",
+                                                textAlign: "center",
+                                                display: "block",
+                                                padding: "0.25rem 0.5rem",
+                                            }}
+                                        >
+                                            New
+                                        </Typography>
+                                    )}
+                                </Typography>
+                            </NavLink>
+                        )}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
